cb659abacfa1c7d962798611d48ea56dfe54068a
杨淮生 <codefan@centit.com>
2017-08-04 12:28:57 +0800

    修改bug

--- framework-adapter/src/test/java/com/centit/framework/test/TestJson.java
+++ framework-adapter/src/test/java/com/centit/framework/test/TestJson.java
@@ -2,7 +2,6 @@ package com.centit.framework.test;
 
 import com.alibaba.fastjson.JSON;
 import com.centit.framework.core.common.ResponseMapData;
-import com.centit.framework.core.common.ResponseSingleData;
 
 /**
  * Created by codefan on 17-8-4.
