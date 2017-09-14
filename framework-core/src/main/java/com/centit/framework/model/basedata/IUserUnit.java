9700cb1a2f1fb88492e6256a63a05c4bd3a96b70
杨淮生 <codefan@centit.com>
2017-09-14 09:38:56 +0800

    类 UserUnit中添加排序号

--- framework-core/src/main/java/com/centit/framework/model/basedata/IUserUnit.java
+++ framework-core/src/main/java/com/centit/framework/model/basedata/IUserUnit.java
@@ -10,30 +10,36 @@ public interface IUserUnit{
 	 * 关联关系主键
 	 * @return 关联关系主键
 	 */
-	 String getUserUnitId();
+	String getUserUnitId();
 	/**
 	 * 用户编码，是用户的主键
 	 * @return 用户编码，是用户的主键
 	 */
-     String getUserCode();
+    String getUserCode();
     /**
 	 * 机构代码 是机构的主键
 	 * @return 机构代码 是机构的主键
 	 */
-	 String getUnitCode();
+	String getUnitCode();
 	/**
      * 是否为主机构 T:主机构 F：辅机构
      * @return 是否为主机构 T:主机构 F：辅机构
      */
-     String getIsPrimary();
+    String getIsPrimary();
     /**
      * 用户在本机构的岗位
      * @return 用户在本机构的岗位
      */
-     String getUserStation();
+    String getUserStation();
     /**
      * 用户在本机构的行政职务
      * @return 用户在本机构的行政职务
      */
-     String getUserRank();
+    String getUserRank();
+
+	/**
+	 * 用户在本单位的排序号
+	 * @return 排序号
+	 */
+	Long getUserOrder();
 }
