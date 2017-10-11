c26dbe7f724d88cfc2cc990dd76bc73270a34b17
杨淮生 <codefan@centit.com>
2017-10-11 11:31:30 +0800

    修改一个缺陷，system对应的framework下的controller可能是多级目录，所以要用**代替*

--- framework-core/src/main/java/com/centit/framework/config/SystemSpringMvcConfig.java
+++ framework-core/src/main/java/com/centit/framework/config/SystemSpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * Created by zou_wy on 2017/3/29.
  */
-@ComponentScan(basePackages = {"com.centit.framework.*.controller"},
+@ComponentScan(basePackages = {"com.centit.framework.**.controller"},
                includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
                useDefaultFilters = false)
 public class SystemSpringMvcConfig extends BaseSpringMvcConfig {
