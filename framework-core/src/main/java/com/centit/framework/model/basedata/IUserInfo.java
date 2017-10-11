a594776c91f1e9b26c36e6091c2c6059eb8edce7
杨淮生 <codefan@centit.com>
2017-10-11 09:49:52 +0800

    添加注解 @JSONField(serialize = false)

--- framework-core/src/main/java/com/centit/framework/model/basedata/IUserInfo.java
+++ framework-core/src/main/java/com/centit/framework/model/basedata/IUserInfo.java
@@ -1,5 +1,7 @@
 package com.centit.framework.model.basedata;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 import java.util.List;
 
 /**
@@ -7,65 +9,66 @@ import java.util.List;
  * 系统用户信息
  * @author codefan@sina.com
  */
- public interface IUserInfo{
+public interface IUserInfo{
     /**
-     * 用户编码，是用户的主键
-     * @return 用户编码，是用户的主键
-     */
-     String getUserCode();
-    
-    /**
-     *  String getUserPin()
-     * @return getUserPin
-     */
-     String getUserPin();
-    /**
-     * 用户是否有效 T/F/A  T 正常 ， F 禁用,A为新建可以删除
-     * @return 用户是否有效 T/F/A  T 正常 ， F 禁用,A为新建可以删除
-     */
-     String getIsValid();
+    * 用户编码，是用户的主键
+    * @return 用户编码，是用户的主键
+    */
+    String getUserCode();
 
     /**
-     * 用户名称  ，和 getUsername()不同后者返回的是用户登录名称
-     * @return 用户名称
-     */
-     String getUserName();
-    
+    *  String getUserPin()
+    * @return getUserPin
+    */
+    @JSONField(serialize = false)
+    String getUserPin();
     /**
-     * 用户登录名 同 getUsername
-     * @return 用户登录名
-     */
-     String getLoginName();
+    * 用户是否有效 T/F/A  T 正常 ， F 禁用,A为新建可以删除
+    * @return 用户是否有效 T/F/A  T 正常 ， F 禁用,A为新建可以删除
+    */
+    String getIsValid();
+
     /**
-     * 用户默认机构（主机构）代码
-     * @return 用户默认机构（主机构）代码
-     */
-     String getPrimaryUnit();
+    * 用户名称  ，和 getUsername()不同后者返回的是用户登录名称
+    * @return 用户名称
+    */
+    String getUserName();
+
     /**
-     * 用户类别，各个业务系统自定义类别信息
-     * @return 用户类别，各个业务系统自定义类别信息
-     */
-     String getUserType(); 
-    
+    * 用户登录名 同 getUsername
+    * @return 用户登录名
+    */
+    String getLoginName();
     /**
-     * 用户注册邮箱
-     * @return 用户注册邮箱
-     */
-     String getRegEmail(); 
+    * 用户默认机构（主机构）代码
+    * @return 用户默认机构（主机构）代码
+    */
+    String getPrimaryUnit();
     /**
-     * 用户注册手机号码
-     * @return 用户注册手机号码
-     */
-     String getRegCellPhone(); 
-    
+    * 用户类别，各个业务系统自定义类别信息
+    * @return 用户类别，各个业务系统自定义类别信息
+    */
+    String getUserType();
+
     /**
-     * 用户排序号
-     * @return 用户排序号
-     */
-     Long getUserOrder();
+    * 用户注册邮箱
+    * @return 用户注册邮箱
+    */
+    String getRegEmail();
     /**
-     * 获取用户归属机构关系
-     * @return 获取用户归属机构关系
-     */
-     List<? extends IUserUnit> getUserUnits();
+    * 用户注册手机号码
+    * @return 用户注册手机号码
+    */
+    String getRegCellPhone();
+
+    /**
+    * 用户排序号
+    * @return 用户排序号
+    */
+    Long getUserOrder();
+    /**
+    * 获取用户归属机构关系
+    * @return 获取用户归属机构关系
+    */
+    List<? extends IUserUnit> getUserUnits();
 }
