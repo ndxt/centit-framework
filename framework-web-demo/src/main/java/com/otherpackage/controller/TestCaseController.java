06e804bbe73054e5fd6bf771daf6b7d3493df7f2
杨淮生 <codefan@centit.com>
2018-01-15 10:13:53 +0800

    添加 Server sent event 相关的测试代码

--- framework-web-demo/src/main/java/com/otherpackage/controller/TestCaseController.java
+++ framework-web-demo/src/main/java/com/otherpackage/controller/TestCaseController.java
@@ -6,15 +6,13 @@ import com.centit.framework.core.controller.BaseController;
 import com.centit.msgpusher.msgpusher.po.SimplePushMessage;
 import com.centit.msgpusher.msgpusher.po.SimplePushMsgPoint;
 import com.centit.msgpusher.msgpusher.websocket.SocketMsgPusher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/test")
@@ -41,6 +39,39 @@ public class TestCaseController extends BaseController {
         } catch (Exception e) {
             return ResponseSingleData.makeResponseData("Error:" + e.getLocalizedMessage());
         }
+    }
 
+    /**
+     * 测试 H5 新特性 server-sent-event
+     * @param response HttpServletResponse
+     */
+    @RequestMapping(value = "/sse")
+    public void serverSentEvent(HttpServletResponse response)
+    {
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            for(int i=0;i<10; i++) {
+                PrintWriter writer = response.getWriter();
+                //这里需要\n\n，必须要，不然前台接收不到值,键必须为data
+                writer.write("data "+ i + " : 中文测试 \n\n");
+                writer.flush();
+                Thread.sleep(3000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value="/sse2",produces="text/event-stream")
+    public @ResponseBody String serverSentEvent2() {
+        Random r = new Random();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //这里需要\n\n，必须要，不然前台接收不到值,键必须为data
+        return "data:Testing 1,2,3" + r.nextInt() +"\n\n";
     }
 }
