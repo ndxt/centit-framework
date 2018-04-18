188e2aafbe67101a1bc5461bd1eb0545140c01e6
杨淮生 <codefan@centit.com>
2018-04-18 11:16:55 +0800

    修改参数名称

--- framework-config/src/main/java/com/centit/framework/config/SecurityCasCondition.java
+++ framework-config/src/main/java/com/centit/framework/config/SecurityCasCondition.java
@@ -2,20 +2,14 @@ package com.centit.framework.config;
 
 import com.centit.framework.common.SysParametersUtils;
 import com.centit.support.algorithm.StringRegularOpt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 /**
  * Created by zou_wy on 2017/4/18.
  */
 public class SecurityCasCondition implements Condition {
-
-    @Autowired
-    protected Environment env;
-
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         boolean isCas = StringRegularOpt.isTrue(SysParametersUtils.getStringValue("login.cas.enable"));
