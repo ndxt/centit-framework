6e025f9f3f7575c235cd8fb22efc0234943dcf1d
杨淮生 <codefan@centit.com>
2018-06-13 11:25:50 +0800

    删除没有必要的依赖

--- framework-web-demo/src/main/java/com/otherpackage/controller/TestCaseController.java
+++ framework-web-demo/src/main/java/com/otherpackage/controller/TestCaseController.java
@@ -1,13 +1,17 @@
 package com.otherpackage.controller;
 
 import com.centit.framework.common.ResponseData;
+import com.centit.framework.common.ResponseMapData;
 import com.centit.framework.common.ResponseSingleData;
 import com.centit.framework.core.controller.BaseController;
+import com.centit.support.algorithm.ByteBaseOpt;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.codec.Hex;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
@@ -20,6 +24,51 @@ public class TestCaseController extends BaseController {
         return ResponseSingleData.makeResponseData("hello "+userName+" !");
     }
 
+    @GetMapping("/hex/{rawhex}")
+    public ResponseData hexConvert(@PathVariable String rawhex,Boolean hf){
+        try {
+            String hexStr = rawhex.toUpperCase();
+            if (hexStr.length() % 2 == 1) {
+                hexStr = "0" + hexStr;
+            }
+            int len = hexStr.length() > 8 ? 8 : 4;
+
+            byte[] buf = new byte[len];
+            //hex = hex.toUpperCase();
+            for (int i = 0; i < len; i++) {
+                buf[i] = 0;
+            }
+
+            byte[] result = Hex.decode(hexStr);
+            int rlen = result.length;
+            if (hf != null && !hf) {
+                for (int i = 0; i < rlen && i < len; i++) {
+                    buf[len - 1 - i] = result[i];
+                }
+            } else {
+                for (int i = 0; i < rlen && i < len; i++) {
+                    buf[len - rlen + i] = result[i];
+                }
+            }
+
+            ResponseMapData responseData = new ResponseMapData();
+
+            if (len < 8) {
+                responseData.addResponseData("int", ByteBaseOpt.readInt32(buf, 0));
+                responseData.addResponseData("float", ByteBaseOpt.readFloat(buf, 0));
+                responseData.addResponseData("Hex20180612", Hex.encode(ByteBaseOpt.castObjectToBytes(20180612)));
+            } else {
+                responseData.addResponseData("long", ByteBaseOpt.readLong(buf, 0));
+                responseData.addResponseData("double", ByteBaseOpt.readDouble(buf, 0));
+                responseData.addResponseData("Hex20180612105706", Hex.encode(ByteBaseOpt.castObjectToBytes(20180612105706L)));
+            }
+            return responseData;
+        }catch (RuntimeException re){
+            return ResponseData.makeErrorMessage(re.getLocalizedMessage());
+        }
+
+
+    }
     /**
      * 测试 H5 新特性 server-sent-event
      * @param response HttpServletResponse
