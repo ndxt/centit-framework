1191521b51f2009226a0522080a9f9253daecbf4
codefan <codefan@sina.com>
2019-09-26 14:55:54 +0800

    完善 接口

--- framework-adapter/src/main/java/com/centit/framework/model/adapter/MessageSender.java
+++ framework-adapter/src/main/java/com/centit/framework/model/adapter/MessageSender.java
@@ -2,6 +2,11 @@ package com.centit.framework.model.adapter;
 
 import com.centit.framework.model.basedata.NoticeMessage;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 系统消息发送接口
  * 系统的内置的两个发送消息的实现,可以通过以下代码引入
@@ -25,7 +30,23 @@ public interface MessageSender {
      * @param message 消息主体
      * @return "OK" 表示成功，其他的为错误信息
      */
-    String sendMessage( String sender, String receiver, NoticeMessage message);
+    String sendMessage(String sender, String receiver, NoticeMessage message);
+
+    /**
+     * 批量发送内部系统消息
+     *
+     * @param sender     发送人内部用户编码
+     * @param receivers   接收人内部用户编码
+     * @param message 消息主体
+     * @return "OK" 表示成功，其他的为错误信息
+     */
+    default List<String> sendMessage(String sender, Collection<String> receivers, NoticeMessage message){
+        List<String> res = new ArrayList<>(receivers.size());
+        for (String receiver : receivers){
+            res.add(sendMessage(sender, receiver, message));
+        }
+        return res;
+    }
 
     /**
      * 发送内部系统消息
