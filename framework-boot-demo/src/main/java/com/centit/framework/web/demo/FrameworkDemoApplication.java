d23ea0ca65f9a8c4f62a1b7b45a93e207f701117
codefan <codefan@sina.com>
2019-11-07 17:59:33 +0800

    添加配置类 方法

--- framework-boot-demo/src/main/java/com/centit/framework/web/demo/FrameworkDemoApplication.java
+++ framework-boot-demo/src/main/java/com/centit/framework/web/demo/FrameworkDemoApplication.java
@@ -2,7 +2,7 @@ package com.centit.framework.web.demo;
 
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.centit.framework.core.controller.WrapUpResponseBodyReturnValueHandler;
+import com.centit.framework.core.controller.MvcConfigUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -12,11 +12,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -34,24 +31,7 @@ public class FrameworkDemoApplication  extends WebMvcConfigurerAdapter implement
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
-        RequestMappingHandlerAdapter requestMappingHandlerAdapter =
-            applicationContext.getBean(RequestMappingHandlerAdapter.class);
-
-        List<HandlerMethodReturnValueHandler> sortedHandlers = new ArrayList<>(20);
-        List<HandlerMethodReturnValueHandler> defaultHandlers = requestMappingHandlerAdapter.getReturnValueHandlers();
-
-        // 建议都使用框架的这个注解处理，为了提高性能可以放在最前面：
-        sortedHandlers.add(new WrapUpResponseBodyReturnValueHandler(fastJsonHttpMessageConverter));
-        sortedHandlers.addAll(defaultHandlers);
-        // 下面的代码式 放到 Spring 定义的  Annotation-based 组中 排在 sortedHandlers 后面
-        /*for(HandlerMethodReturnValueHandler handler : defaultHandlers ){
-            sortedHandlers.add(handler);
-            if(handler instanceof RequestResponseBodyMethodProcessor){
-                sortedHandlers.add(new WrapUpResponseBodyReturnValueHandler(fastJsonHttpMessageConverter()));
-            }
-        }*/
-        requestMappingHandlerAdapter.setReturnValueHandlers(sortedHandlers);
+        MvcConfigUtil.setApplicationContext(applicationContext, fastJsonHttpMessageConverter);
     }
 
     public static void main(String[] args) {
