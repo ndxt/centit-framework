f206c4927036d73e5022b60c0a9353b227cf6537
codefan <codefan@sina.com>
2019-12-09 13:38:41 +0800

    整理代码

--- framework-web-demo/src/main/java/com/centit/framework/web/demo/config/SwaggerConfig.java
+++ framework-web-demo/src/main/java/com/centit/framework/web/demo/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
