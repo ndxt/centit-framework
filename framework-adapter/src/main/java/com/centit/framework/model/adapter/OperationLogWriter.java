c3047f2b34550f9be6abccbcaac87f67af1c0fa6
codefan <codefan@sina.com>
2020-03-03 10:26:56 +0800

    在日志接口中 添加额外的查询接口

--- framework-adapter/src/main/java/com/centit/framework/model/adapter/OperationLogWriter.java
+++ framework-adapter/src/main/java/com/centit/framework/model/adapter/OperationLogWriter.java
@@ -3,19 +3,30 @@ package com.centit.framework.model.adapter;
 import com.centit.framework.model.basedata.OperationLog;
 
 import java.util.List;
+import java.util.Map;
 
 /**
- * 
+ *
  * 操作日志持久化接口，可以单独写入一个日志，和批量写入一个数组
- * 
+ *
  * @author codefan
  * /@create 2015年10月14日
  * /@version
  */
+//默认不支持日志的查询操作
 public interface OperationLogWriter {
 
     void save(OperationLog optLog);
 
     void save(List<OperationLog> optLogs);
 
+    default List<? extends OperationLog>
+        listOptLog(String optId, Map<String, Object> filterMap, int startPos, int maxRows){
+        return null;
+    }
+
+    default int
+        countOptLog(String optId, Map<String, Object> filter){
+        return 0;
+    }
 }
