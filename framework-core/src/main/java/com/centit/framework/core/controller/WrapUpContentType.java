cbfe33756c7e87e8d2f8424b62d7c0bd297ff750
codefan <codefan@sina.com>
2020-03-12 15:16:17 +0800

    添加文件说明

--- framework-core/src/main/java/com/centit/framework/core/controller/WrapUpContentType.java
+++ framework-core/src/main/java/com/centit/framework/core/controller/WrapUpContentType.java
@@ -4,12 +4,12 @@ package com.centit.framework.core.controller;
  * 包装器内容类别
  */
 public enum WrapUpContentType {
-    DATA, // json data
-    RAW,
-    JAVASCRIPT,
-    IMAGE,
-    XML,
-    HTML,
-    FILE,
+    DATA, // json data，默认属性，返回json格式的数据
+    RAW,  // 返回函数返回值的元素数据，如果不是String类型，转换为String类型，复制类型转换为json
+    JAVASCRIPT, // 返回脚本
+    IMAGE, // 返回图片流
+    XML,  //将对象转换为xml返回
+    HTML, // 返回html文本
+    FILE, // 返回文件流
     MAP_DICT // 将对象中的 DictionaryMap 注解 映射为 对应的字段添加到json
 }
