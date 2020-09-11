b40a934fb84cedec39ed19996c50945401f1ca9f
codefan <codefan@sina.com>
2020-09-11 15:17:37 +0800

    重构代码

---
+++ framework-adapter/src/main/java/com/centit/framework/model/adapter/UserUnitFilterCalcContextFactory.java
@@ -0,0 +1,9 @@
+package com.centit.framework.model.adapter;
+
+/**
+ * 用于构建计算权限引擎的上下文环境
+ *
+ */
+public interface UserUnitFilterCalcContextFactory {
+    UserUnitFilterCalcContext createCalcContext();
+}
