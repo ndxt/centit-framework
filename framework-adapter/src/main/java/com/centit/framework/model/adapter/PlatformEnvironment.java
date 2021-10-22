147e6025d7cef99ef088307ba9b07f613583179f
zhf <zhf@centit.com>
2021-10-22 14:38:28 +0800

    增加更新业务接口

--- framework-adapter/src/main/java/com/centit/framework/model/adapter/PlatformEnvironment.java
+++ framework-adapter/src/main/java/com/centit/framework/model/adapter/PlatformEnvironment.java
@@ -151,6 +151,9 @@ public interface PlatformEnvironment {
     default IOptInfo addOptInfo(IOptInfo optInfo) {
         return null;
     }
+    default IOptInfo updateOptInfo(IOptInfo optInfo) {
+        return null;
+    }
 
     /**
      * 获取租户下用户所有角色
