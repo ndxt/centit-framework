d68674acb737334a4df0d736eab221183d990784
zhou_c <zhou_c@centit.com>
2021-11-04 14:41:07 +0800

    修改类名

--- framework-dubbo-client-config/src/main/java/com/centit/framework/dubbo/config/DubboClientConfig.java
+++ framework-dubbo-client-config/src/main/java/com/centit/framework/dubbo/config/DubboClientConfig.java
@@ -1,12 +1,11 @@
 package com.centit.framework.dubbo.config;
+
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ConsumerConfig;
-import org.apache.dubbo.config.ProtocolConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
