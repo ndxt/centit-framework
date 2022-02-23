d2713f6c19f2784dfe94471014e06106ffa74553
tian_y <tian_y@centit.com>
2022-02-23 15:23:33 +0800

    回滚

--- framework-config/src/main/java/com/centit/framework/config/SpringSecurityDaoConfig.java
+++ framework-config/src/main/java/com/centit/framework/config/SpringSecurityDaoConfig.java
@@ -1,7 +1,6 @@
 package com.centit.framework.config;
 
 import com.centit.framework.security.AjaxAuthenticationEntryPoint;
-import com.centit.framework.security.DaoAndLdapAuthenticationProvider;
 import com.centit.framework.security.PretreatmentAuthenticationProcessingFilter;
 import com.centit.support.algorithm.BooleanBaseOpt;
 import com.centit.support.algorithm.NumberBaseOpt;
@@ -106,7 +105,7 @@ public class SpringSecurityDaoConfig extends SpringSecurityBaseConfig {
 
     @Override
     protected AuthenticationProvider getAuthenticationProvider() {
-        DaoAndLdapAuthenticationProvider authenticationProvider = new DaoAndLdapAuthenticationProvider();
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setHideUserNotFoundExceptions(false);
         authenticationProvider.setUserDetailsService(centitUserDetailsService);
         /*if( passwordEncoder instanceof org.springframework.security.authentication.encoding.PasswordEncoder) {
