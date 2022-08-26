33d4b183032105f571ed960972e5e4fc31695c10
codefan <codefan@sina.com>
2022-08-26 14:42:07 +0800

    添加 密码强度验证函数

--- framework-adapter/src/main/java/com/centit/framework/security/model/CentitPasswordEncoder.java
+++ framework-adapter/src/main/java/com/centit/framework/security/model/CentitPasswordEncoder.java
@@ -1,11 +1,45 @@
 package com.centit.framework.security.model;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by codefan on 17-1-20.
  */
 @SuppressWarnings("deprecation")
 public interface CentitPasswordEncoder {
+
+    /**
+     *
+     * @param password 密码
+     * @param minLength 最小长度
+     * @return 返回值 <0 不符合要求， 1～4 之间为密码强度
+     */
+    static int checkPasswordStrength(String password, int minLength){
+        if(StringUtils.isNotBlank(password)){
+            return -1;
+        }
+        int passwrodLen = password.length();
+        int mathLength = passwrodLen>=minLength? 1 : -1;
+        int hasDigit = 0, hasLowLetter = 0, hasUpLetter = 0, hasOtherLetter = 0;
+        for(int i=0; i<passwrodLen; i++){
+            char c = password.charAt(i);
+            if(c>='0' && c<='9'){
+                hasDigit = 1;
+            } else if(c>='a' && c<='z'){
+                hasLowLetter = 1;
+            } else if(c>='A' && c<='Z'){
+                hasUpLetter = 1;
+            } else {
+                hasOtherLetter = 1;
+            }
+        }
+        return mathLength * (hasDigit + hasLowLetter + hasUpLetter + hasOtherLetter);
+    }
+
+    static int checkPasswordStrength(String password){
+        return checkPasswordStrength(password, 8);
+    }
 
     String encodePassword(String rawPass, Object salt);
     /**
