a491205ef52470fa47a6b27a5d3c223ab5bf9cd7
codefan <codefan@centit.com>
2024-01-23 10:50:06 +0800

    添加验证方式

--- framework-core/src/main/java/com/centit/framework/security/UserDetailsServiceImpl.java
+++ framework-core/src/main/java/com/centit/framework/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.centit.framework.security;
 import com.centit.framework.model.adapter.PlatformEnvironment;
 import com.centit.framework.model.security.CentitUserDetails;
 import com.centit.framework.model.security.CentitUserDetailsService;
+import com.centit.support.algorithm.CollectionsOpt;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
@@ -10,20 +11,34 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserDetailsServiceImpl implements
     CentitUserDetailsService,
     AuthenticationUserDetailsService<Authentication>
 {
+    private static Pattern pattern = Pattern.compile("[0-9]*");
     private PlatformEnvironment platformEnvironment;
 
     public void setPlatformEnvironment(PlatformEnvironment platformEnvironment) {
         this.platformEnvironment = platformEnvironment;
     }
 
+    //来自 UserDetailsService
     @Override
     public UserDetails loadUserByUsername(String userLoginName) throws UsernameNotFoundException {
-        UserDetails ud=loadDetailsByLoginName(userLoginName);
+        UserDetails ud = null;
+        if(userLoginName.indexOf('@')>=0){//邮箱
+            ud = loadDetailsByRegEmail(userLoginName);
+        } else{
+            Matcher isNum = pattern.matcher(userLoginName);
+            if(userLoginName.length() == 11 && isNum.matches()){
+                ud = loadDetailsByRegCellPhone(userLoginName);
+            }else{
+                ud=loadDetailsByLoginName(userLoginName);
+            }
+        }
         if(ud==null) {
             throw new UsernameNotFoundException("用户名或密码错误");
         }
