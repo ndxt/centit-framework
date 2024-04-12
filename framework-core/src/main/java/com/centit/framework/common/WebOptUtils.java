f565c399b8f98568df350f0746b4b277d51d1974
codefan <codefan@centit.com>
2024-04-12 15:25:37 +0800

    国际化，添加资源项

--- framework-core/src/main/java/com/centit/framework/common/WebOptUtils.java
+++ framework-core/src/main/java/com/centit/framework/common/WebOptUtils.java
@@ -517,8 +517,11 @@ public class WebOptUtils {
         Map<String, String> headersMap = new HashMap<>();
         while(names.hasMoreElements()) {
             String name = names.nextElement();
-            //cookie
-            if(!"Cookie".equalsIgnoreCase(name)) {
+            //accept-language:"zh-CN,zh;q=0.9"
+            //x-auth-token:"e4fcbbb5-db3a-4b4c-a065-a261f7d4d8fa"
+            if("x-auth-token".equalsIgnoreCase(name)) {
+                headersMap.put(name, request.getHeader(name));
+            } else if("accept-language".equalsIgnoreCase(name)) {
                 headersMap.put(name, request.getHeader(name));
             }
         }
